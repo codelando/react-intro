d33b7d9b939eb505e6822195a875aefd53cb83d0
Ezequiel Cortés <codelando@gmail.com>
2019-11-17 13:54:43 -0300

    Adding turns for players

--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -29,15 +29,18 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
 
   handleClick(i) {
-    console.log(this.state.squares);
     // We call .slice() to create a copy of the squares array to modify instead of modifying the existing array.
     const squares = this.state.squares.slice();
-    squares[i] = 'X';
-    this.setState({squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -47,7 +50,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
